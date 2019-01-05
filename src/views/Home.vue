aa90fe950acc166baa3c260f65500e4edbad1434
Ryan Benson <benson.ryan@gmail.com>
2019-01-05 16:55:09 -0600

    Removes vanilla hello world component from home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,11 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Search from "@/components/Search.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
     Search
   }
 };
